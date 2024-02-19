2d2b3edcdd875c8e5dc06e603e0957bc2a6824fa
lukabor <94577648+lukabor@users.noreply.github.com>
2024-02-20 00:51:13 +0100

    fix(sim_inflate): allow arrays in random.choice
    random.choice never supported numpy arrays: https://github.com/python/cpython/issues/100805

--- vaeda/mk_doublets.py
+++ vaeda/mk_doublets.py
@@ -29,7 +29,7 @@ def sim_inflate(X, frac_doublets=None, seeds=[1234, 15232, 3060309]):
     inflated_sze = np.zeros([len(lib_sze)])
     for i, low in enumerate(lib_sze): 
         random.seed(seeds[2])
-        inflated_sze[i] = random.choice(lib_sze[lib_sze>=low])
+        inflated_sze[i] = np.random.choice(lib_sze[lib_sze>=low])
     
     ls = np.sum(res, axis=1)
     sf = inflated_sze / ls
